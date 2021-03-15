cb3f495c66d5f47b1d43fcbc37c6dfba6a03fe1d
lcs100 <1173515033@qq.com>
2021-03-15 11:20:10 +0800

    3

--- chaincfg/params.go
+++ chaincfg/params.go
@@ -515,7 +515,7 @@ var SimNetParams = Params{
 	GenesisBlock:             &simNetGenesisBlock,
 	GenesisHash:              &simNetGenesisHash,
 	PowLimit:                 simNetPowLimit,
-	PowLimitBits:             0x20100000,
+	PowLimitBits:             0x201f0000,
 	BIP0034Height:            0, // Always active on simnet
 	BIP0065Height:            0, // Always active on simnet
 	BIP0066Height:            0, // Always active on simnet
